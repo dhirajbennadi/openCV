9c0769b516db1f6b239062372104543d6bc37cac
dhirajbennadi <89832913+dhirajbennadi@users.noreply.github.com>
2022-01-21 23:40:16 -0700

    Face and eye detection in live video

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 import cv2
+import time
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 # define a video capture object
 vid = cv2.VideoCapture(0,cv2.CAP_DSHOW)
 
@@ -9,8 +13,23 @@ while(True):
     # by frame
     ret, frame = vid.read()
 
+    cv2.imwrite("frame"+".jpg", frame)
+
+    img = cv2.imread('frame.jpg')
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+    for (ex,ey,ew,eh) in eyes:
+        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
     # Display the resulting frame
-    cv2.imshow('frame', frame)
+    cv2.imshow('frame', img)
+
+    #time.sleep(2)
 
     # the 'q' button is set as the
     # quitting button you may use any
